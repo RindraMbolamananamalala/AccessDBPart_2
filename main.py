89e3e781e3b9e96da45ddee6e05cb042efc3db08
RindraIBI <rindraibi@gmail.com>
2022-12-09 00:07:04 +0300

    Main window design & injection

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import time
 
 from PySide2.QtWidgets import *
 
-from PRESENTATION.HMI.ui_AccessDB_main_window import Ui_MainWindow
+
 from PRESENTATION.VIEW.access_db_view import AccessDBView
 from PRESENTATION.CONTROLLER.access_db_controller import AccessDBController
+
+from PRESENTATION.HMI.ui_AccessDB_PII_MainWindow_GUI import Ui_MainWindow
 
 import threading
 
@@ -13,16 +15,32 @@ import threading
 if __name__ == '__main__':
     application = QApplication(sys.argv)
 
+    main_window = QMainWindow()
+
+    ui_main_window = Ui_MainWindow(main_window)
+    ui_main_window.get_main_window().showMaximized();
+
+
     # controller = CRUDFileController()
 
-    main_window = QMainWindow()
-    window = Ui_MainWindow(main_window)
 
-    access_db_view = AccessDBView(window)
 
-    access_db_controller = AccessDBController(access_db_view)
+    """
+    WORKED FOR PI (beginning)
+    """
+    # main_window = QMainWindow()
+    # window = Ui_MainWindow(main_window)
+    #
+    # access_db_view = AccessDBView(window)
+    #
+    # access_db_controller = AccessDBController(access_db_view)
+    #
+    # access_db_controller.get_access_db_view().get_window_ui().get_main_window().showMaximized()
 
-    access_db_controller.get_access_db_view().get_window_ui().get_main_window().showMaximized()
+    """
+    WORKED FOR PI (end)
+    """
+
 
     #
     # view = CRUDFileView(window)
