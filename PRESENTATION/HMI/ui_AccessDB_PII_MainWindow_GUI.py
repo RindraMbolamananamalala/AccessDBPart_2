8dd616a524d5d90989ed055f39f6a331b0b5b8ac
RindraIBI <rindraibi@gmail.com>
2022-12-09 00:11:40 +0300

    Main window design & injection

---
+++ PRESENTATION/HMI/ui_AccessDB_PII_MainWindow_GUI.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'MainWindow_GUIYUWgVT.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+
+    def set_main_window(self, main_window: QMainWindow):
+        """
+
+        :param main_window: The Qt Main Window to be used by the the current Main Window.
+        :return:
+        """
+        self.main_window = main_window
+
+    def get_main_window(self) -> QMainWindow:
+        """
+
+        :return: The Qt Main Window used by the the current Main Window.
+        """
+        return self.main_window
+
+    def __init__(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        self.set_main_window(MainWindow)
+        MainWindow.setFixedSize(1920, 1080)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(0, 0, 1920, 131))
+        self.widget.setStyleSheet(u"background-color: #1c2632;")
+        self.label_red = QLabel(self.widget)
+        self.label_red.setObjectName(u"label_red")
+        self.label_red.setGeometry(QRect(10, 10, 151, 71))
+        font = QFont()
+        font.setPointSize(40)
+        self.label_red.setFont(font)
+        self.label_red.setStyleSheet(u"color: #f7471f;")
+        self.label_cage_scrap_submition = QLabel(self.widget)
+        self.label_cage_scrap_submition.setObjectName(u"label_cage_scrap_submition")
+        self.label_cage_scrap_submition.setGeometry(QRect(160, 10, 881, 71))
+        self.label_cage_scrap_submition.setFont(font)
+        self.label_cage_scrap_submition.setStyleSheet(u"color: #adaeaf;")
+        self.combo_box_team = QComboBox(self.widget)
+        self.combo_box_team.setObjectName(u"combo_box_team")
+        self.combo_box_team.setGeometry(QRect(1360, 20, 241, 51))
+        self.combo_box_team.setStyleSheet(u"background-color: #d2d2d2;\n"
+"border-radius: 10px;\n"
+"color: #393a46;\n"
+"font-family: Century Gothic;\n"
+"font-size: 30px;")
+        self.label_logo = QLabel(self.widget)
+        self.label_logo.setObjectName(u"label_logo")
+        self.label_logo.setGeometry(QRect(1630, 20, 291, 51))
+        self.label_logo.setPixmap(QPixmap(u"RESOURCES\\IMAGES\\logo_350x45_m75percent.png"))
+        self.line_separation_header_content = QFrame(self.widget)
+        self.line_separation_header_content.setObjectName(u"line_separation_header_content")
+        self.line_separation_header_content.setGeometry(QRect(0, 95, 1920, 5))
+        font1 = QFont()
+        font1.setPointSize(5)
+        self.line_separation_header_content.setFont(font1)
+        self.line_separation_header_content.setAutoFillBackground(False)
+        self.line_separation_header_content.setStyleSheet(u"background-color: #262f3d;")
+        self.line_separation_header_content.setLineWidth(0)
+        self.line_separation_header_content.setMidLineWidth(-16)
+        self.line_separation_header_content.setFrameShape(QFrame.HLine)
+        self.line_separation_header_content.setFrameShadow(QFrame.Sunken)
+        self.widget_content = QWidget(self.centralwidget)
+        self.widget_content.setObjectName(u"widget_content")
+        self.widget_content.setGeometry(QRect(0, 100, 1920, 951))
+        self.widget_content.setStyleSheet(u"background-color: #1c2632;")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 1920, 26))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label_red.setText(QCoreApplication.translate("MainWindow", u"RED", None))
+        self.label_cage_scrap_submition.setText(QCoreApplication.translate("MainWindow", u"CAGE SCRAP SUBMITION", None))
+        self.label_logo.setText("")
+    # retranslateUi
+
