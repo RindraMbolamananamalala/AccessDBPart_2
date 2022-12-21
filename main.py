2b93db2049ca399c6ea06d144a61a7529980c28e
RindraIBI <rindraibi@gmail.com>
2022-12-21 21:10:27 +0300

    Preparing the main.py for the delivery

--- main.py
+++ main.py
@@ -1,77 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+main.py: The python file in charge of the Launch of the whole App
+"""
+
+__author__ = "Rindra Mbolamananamalala"
+__email__ = "rindraibi@gmail.com"
+
 import sys
-import time
+
 
 from PySide2.QtWidgets import *
 
-
-from PRESENTATION.HMI.WIDGET.accessdb_pii_content_zone import AccessDBPIIContentZone
-from PRESENTATION.HMI.WIDGET.access_db_pii_content_body_zone import AccessDBPIIContentBodyZone
-from PRESENTATION.HMI.WIDGET.accessdb_pii_content_familije import AccessDBPIIContentFamilije
-from PRESENTATION.HMI.FORM.form_weight_material_ui import FormWeightMaterialUI
-from PRESENTATION.HMI.FORM.form_weights_validation_ui import FormWeightsValidationUI
-
-from PRESENTATION.VIEW.FORM.form_weight_material_view import FormWeightMaterialView
 
 from PRESENTATION.CONTROLLER.accessdb_pii_controller import AccessDBPIIController
 
 if __name__ == '__main__':
     application = QApplication(sys.argv)
-
-    # main_window = QMainWindow()
-    #
-    # ui_main_window = Ui_MainWindow(main_window)
-    # ui_main_window.get_main_window().showMaximized()
-    #
-    # content_zone = AccessDBPIIContentZone(ui_main_window.get_widget_content())
-    #
-    # content_body_zone = AccessDBPIIContentBodyZone(ui_main_window.get_widget_content())
-    #
-    # content_familije = AccessDBPIIContentFamilije(ui_main_window.get_widget_content())
-    #
-    # form_weigh_material = FormWeighMaterialUI(None)
-    # form_weighs_validation = FormWeighsValidationUI(None)
-    #
-    # form_weighs_validation.get_form_weighs_validation().show()
-    #
-    # ui_main_window.set_content(content_zone)
-
-    """
-    PRE VALIDATION
-    """
+    # Preparing the Controller
     controller = AccessDBPIIController()
-
+    # Displaying the Main Window of the Application
     controller.get_accessdb_pii_main_window_view().get_corresponding_hmi().get_main_window().showMaximized()
-    """
-    PRE VALIDATION
-    """
-
-    # controller = CRUDFileController()
-
-    """
-    WORKED FOR PI (beginning)
-    """
-    # main_window = QMainWindow()
-    # window = Ui_MainWindow(main_window)
-    #
-    # access_db_view = AccessDBView(window)
-    #
-    # access_db_controller = AccessDBController(access_db_view)
-    #
-    # access_db_controller.get_access_db_view().get_window_ui().get_main_window().showMaximized()
-
-    """
-    WORKED FOR PI (end)
-    """
-
-    #
-    # view = CRUDFileView(window)
-    #
-    # controller = CRUDFileController(view)
-    #
-
-    # file_event_handler = CRUDFileEventHandler(controller)
-
-    #
-    # controller.get_crud_file_view().get_main_window_ui().get_main_window().show()
-
     sys.exit(application.exec_())
